1bb7f9f1f278b9878b647c4a10c60df20c4ed8d6
Vitaly Petrov <petrovw@yandex.ru>
2023-08-10 13:10:57 +0300

    Second commit

--- Python_Training/less1_palindrom.py
+++ Python_Training/less1_palindrom.py
@@ -1,27 +1,15 @@
 # объявление функции
 def is_palindrome(text):
     text = text.lower()
-    lst = text.split()
-    for el in lst:
-        while '.' in el:
-            lst.remove('.')
-        while ',' in el:
-            lst.remove(',')
-        while '!' in el:
-            lst.remove('!')
-        while '?' in el:
-            lst.remove('?')
-        while '-' in el:
-            lst.remove('-')
-    print(*lst)
-    length = len(lst)
-    len_2 = 0
-    if length % 2 == 0:
-        len_2 = int(length / 2)
-    else:
-        len_2 = int(length / 2)
-    for i in range(len_2):
-        if lst[i] != lst[-i]:
+    lst = list(text)
+    i = 0
+    while i < len(lst):
+        if lst[i] in '.,!? -':
+            del lst[i]
+            continue
+        i += 1
+    for i in range(int(len(lst) / 2)):
+        if lst[i] != lst[-i-1]:
             return False
     return True
 
