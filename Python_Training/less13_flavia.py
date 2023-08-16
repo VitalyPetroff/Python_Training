6cf4b4c2f16b41da4f82fff1bbcb8273536c67c1
Vitaly Petrov <petrovw@yandex.ru>
2023-08-16 22:40:00 +0300

    flavia

--- Python_Training/less13_flavia.py
+++ Python_Training/less13_flavia.py
@@ -1,7 +1,5 @@
-# n = int(input())
-# k = int(input())
-n = 7
-k = 5
+n = int(input())
+k = int(input())
 
 lst = [1] * n
 index = 0
@@ -10,11 +8,8 @@ while sum(lst) > 1:
     while step < k:
         if lst[index] == 1:
             step += 1
+            if step == k:
+                lst[index] = 0
         index = (index + 1) % n
-    lst[index] = 0
-    while lst[index] == 0:
-        index = (index + 1) % n
-    print(*lst, sep='')
-print('================')
-print(index + 1)
+print(lst.index(1) + 1)
 
